8407f326fe16aeca09842aed51a352f780641751
Hriyen Tripura <128646718+HrIyEn07@users.noreply.github.com>
2024-02-24 03:10:28 +0530

    Create backend.tf

---
+++ WebApp-ASP.Net core/terraform-manifests/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  // backend "azurerm" {
+  //   resource_group_name  = "tfstaterg010"
+  //   storage_account_name = "tfstatebackend01"
+  //   container_name       = "tfcontainer"
+  //   key                  = "terraform.tfstate"
+  // }
+}
