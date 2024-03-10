89f42a573bbabdc5f4b50074f7c6e39ea486ab98
Hriyen Tripura <128646718+HrIyEn07@users.noreply.github.com>
2024-03-10 19:12:25 +0530

    Update c2-resource-group.tf

--- WebApp-ASP.Net core/terraform-manifests/c2-resource-group.tf
+++ WebApp-ASP.Net core/terraform-manifests/c2-resource-group.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "myrg" {
   name = "myrg-2"
   location = "East US"
 }
-# This is from github portal 1
-#This is from github portal 2
+
+
 # This is from GitHub!
 # This is from Local system - Naresh-branch
