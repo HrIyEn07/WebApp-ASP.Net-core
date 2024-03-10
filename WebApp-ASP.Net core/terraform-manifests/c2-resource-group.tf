fc45d6806973ea89005a4394798252afdc81e500
Hriyen <Hriyen.tripura@gmail.com>
2024-03-10 19:03:14 +0530

    from dev-branch- conflics

--- WebApp-ASP.Net core/terraform-manifests/c2-resource-group.tf
+++ WebApp-ASP.Net core/terraform-manifests/c2-resource-group.tf
@@ -3,6 +3,9 @@ resource "azurerm_resource_group" "myrg" {
   name = "myrg-2"
   location = "East US"
 }
+# This is from dev-branch 2
+##This is from dev-branch local system
+# This is from GitHub!
+# This is from Local system - Naresh-branch
 
 
-# This is from Dev branch! Work is in progress!
