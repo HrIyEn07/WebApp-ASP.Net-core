3c27833686c631e3e2084bc4352ca56d10be420c
Hriyen <Hriyen.tripura@gmail.com>
2024-03-12 22:28:30 +0530

    file added

--- WebApp-ASP.Net core/terraform-manifests/c2-resource-group.tf
+++ WebApp-ASP.Net core/terraform-manifests/c2-resource-group.tf
@@ -1,6 +1,6 @@
 # Resource-1: Azure Resource Group
 resource "azurerm_resource_group" "myrg" {
-  name = "myrg-2"
+  name = "naresh-rg02"
   location = "East US"
 }
 
--- WebApp-ASP.Net core/terraform-manifests/c3-virtual-network.tf
+++ WebApp-ASP.Net core/terraform-manifests/c3-virtual-network.tf
@@ -1,6 +1,6 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "myvnet" {
-  name                = "myvnet-222"
+  name                = "naresh-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
