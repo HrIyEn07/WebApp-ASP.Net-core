2519f1e2f268df2c87134b4fac6ab638823e6411
Hriyen Tripura <128646718+HrIyEn07@users.noreply.github.com>
2024-03-13 22:07:44 +0530

    Update c2-resource-group.tf

--- WebApp-ASP.Net core/terraform-manifests/c2-resource-group.tf
+++ WebApp-ASP.Net core/terraform-manifests/c2-resource-group.tf
@@ -1,16 +1,10 @@
 # Resource-1: Azure Resource Group
 resource "azurerm_resource_group" "myrg" {
-<<<<<<< HEAD
-  name = "naresh-rg02"
-  location = "East US"
-}
-=======
   name = "for-cherry-pickrg"
   location = "East US"
 }
 
 # This is for blame!
->>>>>>> 36770ac (for cherry-pick)
 
 # file added
 
